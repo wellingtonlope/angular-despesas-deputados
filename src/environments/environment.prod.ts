751e10b50c5e1d044b732887624faa3f8e9367ea
Wellington Lopes <wellington.lopes.tw@gmail.com>
2018-02-05 08:30:28 -0400

    Configuração do firebase para produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAg1ca5iXS-noYanr_rIWuTPxY_FIxKWMQ",
+    authDomain: "deputados-745ce.firebaseapp.com",
+    databaseURL: "https://deputados-745ce.firebaseio.com",
+    projectId: "deputados-745ce",
+    storageBucket: "deputados-745ce.appspot.com",
+    messagingSenderId: "1027503532135"
+  }
 };
